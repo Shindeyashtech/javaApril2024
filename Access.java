5361effaa882e9ee29582c75cd310a630d958b04
Yash shinde <yashshinde.tech@gmail.com>
2024-04-12 11:38:03 +0530

    Access modifiers private protected

---
+++ Access.java
@@ -0,0 +1,19 @@
+import java.io.*; 
+class Encap {
+//protected
+ private int a;
+public void call( ){
+a=10;
+ System.out.println(""+ a);
+}}
+class Access {
+public static void main (String args [])
+{
+Encap e1= new Encap();
+e1.call();
+
+System.out.println( a +""+b);
+
+}
+}
+ 
