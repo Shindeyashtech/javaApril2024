e892cba5d6187270db6fbb84a101b675f0f74935
Yash shinde <yashshinde.tech@gmail.com>
2024-04-15 21:36:23 +0530

    ASTRACT CLASS

---
+++ Abtrac.java
@@ -0,0 +1,18 @@
+abstract class Animal {
+public abstract void animalsound();
+public void sleep(){
+System.out.println("AAHHHH");
+}
+}
+class Pig extends Animal{
+public void animalsound(){
+System.out.println("this pig says : wee wee");
+}
+}
+class Abtrac{
+public static void main(String args[]){
+Pig p1 = new Pig();
+p1.animalsound();
+p1.sleep();
+}
+}
