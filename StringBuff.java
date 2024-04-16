fcb3a4d3f3880a85fdb7d3401d3221b27db1b47b
Yash shinde <yashshinde.tech@gmail.com>
2024-04-16 11:48:48 +0530

    String & StringBuffer thier  method

---
+++ StringBuff.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class StringBuff {
+public static void main(String args [])
+{
+StringBuffer sb1 = new StringBuffer ("hello");
+//sb1.append(" Yash");
+//sb1.insert(0,"home");
+//sb1.replace(1,3,"raj");
+//sb1.lenght();
+//sb1.delete(0,6);
+//sb1.reverse();
+//sb1.capacity();
+StringBuffer sb2 = new StringBuffer ("yash");
+//  insureCapacity deleteCharAt charAt
+System.out.println(sb2.capacity());
+System.out.println(sb2.length());
+}
+}
---
+++ StrinngBuff.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class StringBuff {
+public static void main(String args [])
+{
+StringBuffer sb1 = new StringBuffer ("hello");
+sb1.append("Yash");
+System.out.println(sb1);
+}
+}
