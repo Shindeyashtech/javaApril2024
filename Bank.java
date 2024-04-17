211cbd1c41f0b05c3249edf760fd34da2f235f36
Yash shinde <yashshinde.tech@gmail.com>
2024-04-17 09:25:37 +0530

    Bank Application

---
+++ Bank.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class Acc{
+private String name = "yash";
+private int Id = "1" ;
+private double blanc = 500;
+}
+class Savingsacc extends Acc{
+public void save(){
+if ( acc.name= "yash" || id = 1 ) {
+System.out.println(acc.blanc);
+} else {
+    System.out.println("incorrect");
+	}
+}} 
+public class Bank {
+public static void main (String args[]){
+Savingsacc n1 = new Savingsacc();
+n1.save();  
+}}
