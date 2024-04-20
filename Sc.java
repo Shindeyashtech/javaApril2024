a1e07997bd228cef2560e2b55eea5b5cbfcc327a
Yash shinde <yashshinde.tech@gmail.com>
2024-04-20 12:21:58 +0530

    scanner class and method

---
+++ Sc.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Sc {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("enter num 1");
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        
+        System.out.println("enter num 2");
+
+        // Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+
+        System.out.println("addtion of a+b");
+  
+        int sum = a+b;
+
+        System.out.println(" "+ sum +" ");
+    }
+
+}
---
+++ Sc1.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Sc1 {
+
+public static void main(String[] args) {
+    
+System.out.println("Enter your name ");
+Scanner sc = new Scanner(System.in);
+   String a = sc.next() ;
+System.out.println(" "  + a);
+
+
+}
+    
+}
