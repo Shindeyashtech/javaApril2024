9803848f957733065efb0c929c1ed8791b42a550
Yash shinde <yashshinde.tech@gmail.com>
2024-04-23 20:13:16 +0530

    finally keyword

---
+++ Final.java
@@ -0,0 +1,24 @@
+public class Final {
+    public static void main(String[] args) {
+        
+            try {
+                int smt = 50/0;
+
+
+            } catch (ArithmeticException e) {
+                System.out.println( e);
+                 
+            }
+            finally{
+                System.out.println("yash");
+                    
+
+                
+
+
+            }
+
+
+    }
+    
+}
