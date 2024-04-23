88b7b7e5f169889c3189d7c2c85e538f6c656d62
Yash shinde <yashshinde.tech@gmail.com>
2024-04-23 14:51:33 +0530

    Exception handling with all type

--- Ex.java
+++ Ex.java
@@ -1,4 +1,4 @@
-package exception;
+
 
 public class Ex {
 	
@@ -6,15 +6,31 @@ public class Ex {
 		
 		try {
 			
-			int data = 50/0;
+			//int data = 50/0;
+			//int a[]= new int [5];
+			//a[5]=30/0;
+			//System.out.println(a[10]);
+			Integer[] a = new Integer[5];
+			a[0] = null; // Setting an array element to null
+            int length = a[0].intValue();
 			
 		}
 		catch (ArithmeticException e) {
 			
 			System.out.println(e);
 		}
-		
-		
+catch  ( NullPointerException  e) {
+			
+			System.out.println(e);
+		}
+catch ( ArrayIndexOutOfBoundsException e) {
+			
+			System.out.println(e);
+		}
+catch (Exception e) {
+			
+			System.out.println(e);
+		}
 		System.out.println("oro");
 	}
 	
