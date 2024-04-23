022c9276c0923caea42a48e957b617daff83a9dc
Yash shinde <yashshinde.tech@gmail.com>
2024-04-23 20:12:26 +0530

    nested exception with diff class and get set

---
+++ NestedWithGetSet.java
@@ -0,0 +1,28 @@
+public class NestedWithGetSet {
+    public static void main(String[] args) {
+        try {
+            //int data = 50 / 0;
+            try {
+                // int data = 50/0;
+            } catch (ArithmeticException e) {
+                System.out.println(e);
+            }
+            try {
+                Integer[] a = new Integer[5];
+                a[0] = null; // Setting an array element to null
+                int length = a[0].intValue();
+            } catch (NullPointerException e) {
+                System.out.println(e);
+            }
+            try {
+                // int a[]= new int [5];
+                // System.out.println(a[10]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("oro");
+    }
+}
