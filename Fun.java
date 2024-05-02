fbe8cef370025df6b2a4d96d2d3ec0ce6c0077e6
Yash shinde <yashshinde.tech@gmail.com>
2024-05-02 11:08:37 +0530

    method ref

--- Fun.java
+++ Fun.java
@@ -1,10 +1,24 @@
-interface FunWithAbstract {
-    abstract int cal(int x,int y);
-}
 
+/**
+ * InnerFun
+ */
+class InnerFun {
+    int a = 4, b = 5, c = 5, result, total;
+    public int add() {
+        result = a + b;
+        System.out.println(result);
+        return result;
+    }
+    public int result(int result) {
+        System.out.println(result);
+        return total = result + c;
+    }
+}
 public class Fun {
     public static void main(String[] args) {
-        FunWithAbstract f1 = (int x,int y) -> x * y;
-        System.out.println(f1.cal(4,2));
+        InnerFun i1 = new InnerFun();
+        int D = i1.result();
+        // int b = i1.add();
+        System.out.println(D);
     }
 }
