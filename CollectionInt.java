83a063a456cfb5aa9c848a7cebc7f3b43a7596b7
Yash shinde <yashshinde.tech@gmail.com>
2024-05-12 20:17:52 +0530

    int in collection

---
+++ CollectionInt.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class CollectionInt {
+
+public static void main(String[] args) {
+    ArrayList<Integer> a2 = new ArrayList<Integer>();
+    a2.add(1);
+    Iterator<Integer> i1 = a2.iterator();
+
+    System.out.println(i1.next());
+}    
+}
