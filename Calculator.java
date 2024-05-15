a7064d0b3d601ecbfe6bee652d6ee79d1ec6ff98
Yash shinde <yashshinde.tech@gmail.com>
2024-05-15 09:30:54 +0530

    JunitTest

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package Cal;
+
+public class Calculator {
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
---
+++ TestHere.java
@@ -0,0 +1,23 @@
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+//import org.junit.jupiter.api.Test;
+
+//import Cal.Calculator;
+
+class TestHere {
+
+	@Test
+	void testfindMax(int[] arr) {
+		//fail("Not yet implemented");
+		int max = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+		
+		
+	}
+}
+}
