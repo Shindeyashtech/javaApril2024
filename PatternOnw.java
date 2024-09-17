c2c73ed79e0c017d2335371deee3b11330f1bee1
Yash shinde <yashshinde.tech@gmail.com>
2024-09-17 18:29:12 +0000

    Char A pattern

--- PatternOnw.java
+++ PatternOnw.java
@@ -5,5 +5,19 @@ public class PatternOnw {
     public static void main (String args[])
     {
         System.out.println("hello");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <9; j++) {
+
+                if ((j == 6-i)|| (i == 3&&j<=7)||(j==4+i)) {
+                    
+                    System.out.print("*");
+                }
+                else{ 
+
+                    System.out.println("*");
+                }
+            }
+        }
     }
 }
